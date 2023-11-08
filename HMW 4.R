cd0b7b34c679898b5b61fae1e008981d221cc0ce
Gresse Johanne MSc. <gressj00@jcu.cz>
2023-11-08 10:28:43 +0100

    11/8

--- HMW 4.R
+++ HMW 4.R
@@ -1,18 +1,35 @@
 pollen <- read.delim("clipboard")
 summary(pollen)
+View(pollen)
 
 GLM.1 <- glm(cbind(Picea,Total-Picea)~DegDays, family=binomial, data=pollen)
 summary(GLM.1)
 
+# Creating the second-order polynomial for DegDays
+DegDays_poly <- (pollen$DegDays)^2
 
-GLM.2 <- glm(cbind(Picea,Total-Picea)~DegDays^2, family=binomial, data=pollen)
+# Adding the new variable to the existing dataset
+pollen <- cbind(pollen, DegDays_poly = DegDays_poly)
+
+View(pollen)
+
+GLM.2 <- glm(cbind(Picea,Total-Picea)~ DegDays_poly, family=binomial, data=pollen)
 summary(GLM.2)
 
 
+#second order degdays seem to be better than first order. for both models, the deviance is more than two times larger than df, worrying because observed count of Picea are overdispersed.
+#SO use of quasi binomial
+
+GLM.3 <- glm(cbind(Picea,Total-Picea)~DegDays, family=quasibinomial, data=pollen)
+summary(GLM.3)
+
 #try quasi binomial if overdispersion
+GLM.4 <- glm(cbind(Picea,Total-Picea)~DegDays_poly, family=quasibinomial, data=pollen)
+summary(GLM.4)
 
 
 #Comparison of the two models
-anova(GLM.1, GLM.2, test="Chisq") #all other cases than poisson family gama, etc... test F
+anova(GLM.3, GLM.4, test="F") #F test for quasi binomial
 
-#F test for quasi binomial
+#GRAPHS
+
